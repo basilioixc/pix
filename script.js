454d3146b287d02ab17e131d872112718f835ce4
basilioixc <whatsbas@gmail.com>
2025-03-12 13:53:18 -0300

    Update script.js

--- script.js
+++ script.js
@@ -1,9 +1,4 @@
 document.getElementById("copyPix").addEventListener("click", function() {
     const pixCode = "00020101021126580014br.gov.bcb.pix0136f0501667-7370-4b0b-9f26-4361508747345204000053039865802BR5918ANISIO SILVA LEITE6009SAO PAULO622905251JN0X8Y5DX4EY2Y6WE375MAAN63043073";
 
-    navigator.clipboard.writeText(pixCode).then(() => {
-        alert("Código Pix copiado!");
-    }).catch(err => {
-        console.error("Erro ao copiar:", err);
-    });
 });
